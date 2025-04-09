c733c8f0d48ef64d913463847796809356bdebb3
Kalib <jacobs.kalib@gmail.com>
2025-04-09 16:30:16 +1000

    Updated threshold trigger: digital inputs, debug print statement, if-else decision tree.

--- threshold_trigger.py
+++ threshold_trigger.py
@@ -3,14 +3,16 @@
 
 
 from motor import Motor
-import machine
+from machine import Pin
 import sys
 from ultrasonic import sonic
 from time import sleep
 
-adc_L = machine.ADC(28)
-adc_M = machine.ADC(27)
-adc_R = machine.ADC(26)
+adc_LL = Pin(21, Pin.IN)
+adc_L = Pin(20, Pin.IN)
+adc_M = Pin(19, Pin.IN)
+adc_R = Pin(18, Pin.IN)
+adc_RR = Pin(17, Pin.IN)
 
 motor_right = Motor("right", 8, 9, 7)
 motor_left = Motor("left", 11,12,13)
@@ -19,27 +21,28 @@ ultrasonic_sensor = sonic(17, 16)
 
 while True:
 
-    value_L = adc_L.read_u16()
-    value_M = adc_M.read_u16()
-    value_R = adc_R.read_u16()
+    value_LL = adc_LL.value()
+    value_L = adc_L.value()
+    value_M = adc_M.value()
+    value_R = adc_R.value()
+    value_RR = adc_RR.value()
 
-    try:
-        line_pos = ((value_L*10) + (value_M*0) + (value_R*-10)) / (value_L + value_M + value_R) # Weighted Arithmetic Mean (lab 4, page 8; I'm not explaining it) -KJ
-    except ZeroDivisionError:
-        line_pos = 0
+    motor_left.set_forwards()
+    motor_right.set_forwards()
 
-    # sys.stdout.write(f"{line_orient}mm  from centre.\n") #DEBUG FUNC. -KJ
-
-    dist = ultrasonic_sensor.distance_mm()
-
-    if dist <= 200:
+    if line_pos == 0:
+        motor_left.duty(40)
+        motor_right.duty(40)
+    elif line_pos == 1:
+        motor_left.duty(20)
+        motor_right.duty(40)
+    elif line_pos == -1:
+        motor_left.duty(40)
+        motor_right.duty(20)
+    else:
         motor_left.duty(0)
         motor_right.duty(0)
-        sys.stdout.write("Obstacle detected. Stopping.\n")
-        continue
 
-    thresholds = {0: [motor_left.set_forwards(), motor_right.set_forwards(), motor_left.duty(40), motor_right.duty(40)],
-                  1: [motor_left.set_forwards(), motor_right.set_forwards(), motor_left.duty(20), motor_right.duty(40)],
-                  -1: [motor_left.set_forwards(), motor_right.set_forwards(), motor_left.duty(40), motor_right.duty(20)]}
+    sys.stdout.write(f"Value LL={value_LL:.2f} | Value LL={value_L:.2f} | Value LL={value_M:.2f} | Value LL={value_R:.2f} | Value LL={value_RR:.2f}\n")
 
 
