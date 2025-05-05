eabe3627abfaf4377c0ee45d1cc80bef9cc62d91
Kalib <jacobs.kalib@gmail.com>
2025-05-05 11:54:35 +1000

    Keeping a record

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-### Can follow a line:
-### Will orient left or right, and drive forward
+## NOTE: Minimum & maximum IR read values
+# White mins: [2240, 2416, 2320]
+# Black maxs: [3712, 3968, 4273]
+# Thresholds: [2976, 3192, 3296]
+## TODO: dynamic updating as IR reads new values; set new values so mins and maxes are relevant
 
-
+## NOTE: Various imports
 from motor import Motor
 import machine
 import sys
 from ultrasonic import sonic
 from time import sleep
 
+## NOTE: Pin setups (IR sensors, motors, ultrasonic sensors)
 adc_L = machine.ADC(28)
 adc_M = machine.ADC(27)
 adc_R = machine.ADC(26)
 
 motor_right = Motor("right", 8, 9, 7)
-motor_left = Motor("left", 11, 12, 13)
+motor_left = Motor("left", 11,12,13)
 
 ultrasonic_sensor = sonic(17, 16)
 
 while True:
+    # NOTE:
+    vL = adc_L.read_u16()
+    vM = adc_M.read_u16()
+    vR = adc_R.read_u16()
+    total = vL + vM + vR
 
-    value_L = adc_L.read_u16()
-    value_M = adc_M.read_u16()
-    value_R = adc_R.read_u16()
-
-    line_pos = ((value_L * 10) + (value_M * 0) + (value_R * -10)) / (value_L + value_M + value_R)
-
-    motor_left.set_forwards()
-    motor_right.set_forwards()
-
-    if line_pos == 0:
-        motor_left.duty(40)
-        motor_right.duty(40)
-    elif line_pos == 1:
-        motor_left.duty(20)
-        motor_right.duty(40)
-    elif line_pos == -1:
-        motor_left.duty(40)
-        motor_right.duty(20)
+    if total == 0:
+        error = 0
     else:
-        motor_left.duty(0)
-        motor_right.duty(0)
-
-
+        # NOTE: [–1…+1], positive = line is to the left, negative = to the right
+        error = (vL - vR) / total
--- threshold_trigger.py
+++ threshold_trigger.py
@@ -3,33 +3,109 @@
 
 
 from motor import Motor
-from machine import Pin
+import machine
 import sys
 from ultrasonic import sonic
 from time import sleep
+from enum import IntEnum
 
-adc_LL = Pin(21, Pin.IN)
-adc_L = Pin(20, Pin.IN)
-adc_M = Pin(19, Pin.IN)
-adc_R = Pin(18, Pin.IN)
-adc_RR = Pin(17, Pin.IN)
+# --- Configuration --- #
+# Digital Sensor Pins
+IR_sensors = [21, 20, 19, 18, 17]
 
+# Motor Setup
 motor_right = Motor("right", 8, 9, 7)
 motor_left = Motor("left", 11,12,13)
 
+# Ultrasonic Sensor Setup
 ultrasonic_sensor = sonic(17, 16)
 
+# --- Speed Constants
+small = 30
+medium = 50
+large = 80
+delta = 20
+
+# --- State Definition ---
+class State(IntEnum):
+    straight = 0
+    slight_left = 1
+    sharp_left = 2
+    slight_right = 3
+    sharp_right = 4
+    search_left = 5
+    search_right = 6
+
+# --- Initialization ---
+sensors = [machine.Pin(pin, machine.Pin.IN) for pin in IR_sensors]
+last_known = State.straight
+current = State.straight
+
+# --- Helpers ---
+def read_sensors():
+    return [s.value() for s in sensors]
+
+def update_state():
+    global current, last_known
+    bits = read_sensors()
+    pattern = (bits[0]<<4 | (bits[1]<<3) | (bits[2]<<2) | (bits[3]<<1) | bits[4])
+
+    if pattern in (0b00100, 0b01110):
+        new = State.straight
+    elif pattern in (0b01000, 0b11000):
+        new = State.slight_left
+    elif pattern in (0b10000, 0b11100):
+        new = State.sharp_left
+    elif pattern in (0b00010, 0b00011):
+        new = State.slight_right
+    elif pattern in (0b00001, 0b00111):
+        new = State.sharp_right
+    elif pattern == 0b00000:
+        # choose search dir based on last_known
+        if last_known in (State.sharp_left, State.slight_left):
+            new = State.search_left
+        else:
+            new = State.search_right
+    else:
+        # fallback
+        new = State.straight
+
+    if new not in (State.search_left, State.search_right):
+        last_known = new
+    current = new
+
+def apply_state(state):
+    """Drive motors according to the chosen state."""
+    if state == State.straight:
+        motor_left.set_forwards();  motor_left.duty(medium)
+        motor_right.set_forwards(); motor_right.duty(medium)
+
+    elif state == State.slight_left:
+        motor_left.set_forwards();  motor_left.duty(medium - delta)
+        motor_right.set_forwards(); motor_right.duty(medium + delta)
+
+    elif state == State.sharp_left:
+        motor_left.set_backwards(); motor_left.duty(small)
+        motor_right.set_forwards();  motor_right.duty(large)
+
+    elif state == State.slight_right:
+        motor_left.set_forwards();  motor_left.duty(medium + delta)
+        motor_right.set_forwards(); motor_right.duty(medium - delta)
+
+    elif state == State.sharp_right:
+        motor_left.set_forwards();  motor_left.duty(large)
+        motor_right.set_backwards();motor_right.duty(small)
+
+    elif state == State.search_left:
+        motor_left.set_backwards(); motor_left.duty(small)
+        motor_right.set_forwards();  motor_right.duty(small)
+
+    elif state == State.search_right:
+        motor_left.set_forwards();  motor_left.duty(small)
+        motor_right.set_backwards();motor_right.duty(small)
+
+# --- Main Loop ---
 while True:
-
-    value_LL = adc_LL.value()
-    value_L = adc_L.value()
-    value_M = adc_M.value()
-    value_R = adc_R.value()
-    value_RR = adc_RR.value()
-
-    motor_left.set_forwards()
-    motor_right.set_forwards()
-
-    sys.stdout.write(f"Value LL={value_LL:.2f} | Value L={value_L:.2f} | Value M={value_M:.2f} | Value R={value_R:.2f} | Value RR={value_RR:.2f}\n")
-
-
+    update_state()
+    apply_state(current)
+    time.sleep(loop_delay)
